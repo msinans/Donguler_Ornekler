e5122cb14beacad2d1f3b1bd673bb1cc2f32a6f4
m.sinans <msomercikoglu@gmail.com>
2023-03-23 15:05:54 +0300

    23.03.2023

--- Donguler_Ornekler/Form1.Designer.cs
+++ Donguler_Ornekler/Form1.Designer.cs
@@ -35,22 +35,43 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox9 = new System.Windows.Forms.GroupBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.button9 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.groupBox9.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -130,6 +151,23 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Bir Sayı Giriniz:";
+            // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -151,23 +189,6 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "label3";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 18);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Bir Sayı Giriniz:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(118, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(86, 22);
-            this.textBox1.TabIndex = 3;
-            // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.listBox1);
@@ -178,6 +199,15 @@
             this.groupBox4.TabIndex = 5;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "groupBox4";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(26, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 1;
             // 
             // button4
             // 
@@ -190,23 +220,13 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(26, 21);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 94);
-            this.listBox1.TabIndex = 1;
-            // 
             // groupBox5
             // 
-            this.groupBox5.Controls.Add(this.listBox3);
             this.groupBox5.Controls.Add(this.listBox2);
             this.groupBox5.Controls.Add(this.button5);
             this.groupBox5.Location = new System.Drawing.Point(12, 192);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(264, 268);
+            this.groupBox5.Size = new System.Drawing.Size(134, 268);
             this.groupBox5.TabIndex = 6;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "groupBox5";
@@ -223,7 +243,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(88, 193);
+            this.button5.Location = new System.Drawing.Point(20, 192);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(85, 31);
             this.button5.TabIndex = 0;
@@ -231,20 +251,196 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.listBox3);
+            this.groupBox6.Controls.Add(this.textBox2);
+            this.groupBox6.Controls.Add(this.label5);
+            this.groupBox6.Controls.Add(this.button6);
+            this.groupBox6.Location = new System.Drawing.Point(158, 192);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(236, 268);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "groupBox6";
+            // 
             // listBox3
             // 
             this.listBox3.FormattingEnabled = true;
             this.listBox3.ItemHeight = 15;
-            this.listBox3.Location = new System.Drawing.Point(132, 21);
+            this.listBox3.Location = new System.Drawing.Point(110, 58);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(120, 154);
-            this.listBox3.TabIndex = 2;
+            this.listBox3.Size = new System.Drawing.Size(120, 199);
+            this.listBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Bir Sayı Giriniz:";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(6, 86);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 31);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Listele";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.listBox4);
+            this.groupBox7.Controls.Add(this.textBox3);
+            this.groupBox7.Controls.Add(this.label6);
+            this.groupBox7.Controls.Add(this.button7);
+            this.groupBox7.Location = new System.Drawing.Point(400, 192);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(236, 268);
+            this.groupBox7.TabIndex = 8;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "groupBox7";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(110, 58);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 199);
+            this.listBox4.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(86, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Bir Sayı Giriniz:";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(6, 86);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 31);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Listele";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.Controls.Add(this.listBox5);
+            this.groupBox8.Controls.Add(this.button8);
+            this.groupBox8.Controls.Add(this.textBox4);
+            this.groupBox8.Controls.Add(this.label7);
+            this.groupBox8.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox8.Location = new System.Drawing.Point(642, 192);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(226, 257);
+            this.groupBox8.TabIndex = 9;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "groupBox8";
+            // 
+            // listBox5
+            // 
+            this.listBox5.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 15;
+            this.listBox5.Location = new System.Drawing.Point(113, 52);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(107, 199);
+            this.listBox5.TabIndex = 4;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(9, 89);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(81, 33);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "faktoriyel";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(9, 58);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(81, 25);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 18);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Bir Sayı Giriniz:";
+            // 
+            // groupBox9
+            // 
+            this.groupBox9.Controls.Add(this.listBox6);
+            this.groupBox9.Controls.Add(this.button9);
+            this.groupBox9.Location = new System.Drawing.Point(708, 12);
+            this.groupBox9.Name = "groupBox9";
+            this.groupBox9.Size = new System.Drawing.Size(173, 174);
+            this.groupBox9.TabIndex = 10;
+            this.groupBox9.TabStop = false;
+            this.groupBox9.Text = "For içinde for";
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 15;
+            this.listBox6.Location = new System.Drawing.Point(26, 21);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(120, 94);
+            this.listBox6.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(43, 137);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(85, 31);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Listele";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(893, 514);
+            this.Controls.Add(this.groupBox9);
+            this.Controls.Add(this.groupBox8);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -262,6 +458,13 @@
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.groupBox8.ResumeLayout(false);
+            this.groupBox8.PerformLayout();
+            this.groupBox9.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -283,9 +486,26 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.GroupBox groupBox5;
-        private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.GroupBox groupBox9;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- Donguler_Ornekler/Form1.cs
+++ Donguler_Ornekler/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Donguler_Ornekler
@@ -16,7 +9,7 @@ namespace Donguler_Ornekler
         {
             InitializeComponent();
         }
-
+        // 10 dan küçük sayıların toplamı
         private void button1_Click(object sender, EventArgs e)
         {
             int toplam = 0;
@@ -26,7 +19,7 @@ namespace Donguler_Ornekler
             }
             label1.Text = toplam.ToString();
         }
-
+        // 10 dan küçük çift sayıların toplamı
         private void button2_Click(object sender, EventArgs e)
         {
             int toplam = 0;
@@ -37,7 +30,7 @@ namespace Donguler_Ornekler
             }
             label2.Text = toplam.ToString();
         }
-
+        // Klavyeden girilecek olan sayı ile 0 arasındaki sayıların toplamı
         private void button3_Click(object sender, EventArgs e)
         {
             int toplam = 0, i, sayi = Convert.ToInt16(textBox1.Text);
@@ -48,7 +41,7 @@ namespace Donguler_Ornekler
             }
             label3.Text = toplam.ToString();
         }
-
+        // 1 ile 10 arasında olup 3 e kalansız bölünen sayılar
         private void button4_Click(object sender, EventArgs e)
         {
             for (int i = 1; i <= 10; i++)
@@ -60,15 +53,62 @@ namespace Donguler_Ornekler
             }
 
         }
-
+        // 100'den küçük olup 5 ve 7 sayılarına kalansız bölünen sayılar
         private void button5_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i <= 100;  i++)
+            for (int i = 1; i <= 100; i++)
             {
-                if (i%5==0 || i%7==0)
+                if (i % 5 == 0 && i % 7 == 0)
                 {
                     listBox2.Items.Add(i);
 
+                }
+            }
+        }
+        // Klavyeden girilecek olan sayının 1'den o sayıya kadar yazdırılması
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(textBox2.Text);
+            for (int i = 1; i <= sayi; i++)
+            {
+                listBox3.Items.Add(i);
+            }
+        }
+        // Klavyeden girilen sayının tam bölenleri
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(textBox3.Text);
+            for (int i = 1; i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    listBox4.Items.Add(i);
+                }
+            }
+        }
+        // Klavyeden girilecek sayının faktoriyeli
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+            int i, sayi = Convert.ToInt16(textBox4.Text);
+            int faktoriyel = 1;
+            for (i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+
+            }
+            listBox5.Items.Add(sayi + "! = " + faktoriyel);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            char[] harf = { 'a', 'b', 'c' };
+            for (int i = 1; i < 6; i++)
+            {
+                listBox6.Items.Add(i);
+                foreach (var h in harf)
+                {
+                    listBox6.Items.Add(h);
                 }
             }
         }
